6d972516af5c2db52a11ce4b9cac1750b0c66695
saloninformationsystem <saloninformationsystem@gmail.com>
2026-09-08 11:43:16 +0900

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@
 
 // システム設定
 const CONFIG = {
-  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzDKi7LdkPlaDaLlS_nN-OKuRJb_z2gVlurZEdyvhunJK9OnvBNRyqNq4oKY4GlR-_u/exec",
+  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzen1XyzC4UBftuKiygAynoORYg9Hs-cJLctOW553FfPyT9tPVSklZQwB61LqjKv31K/exec",
   STORAGE_FIELDS: ["name", "name_kana", "tel", "email"],
   STORAGE_PREFIX: "sis_",
 
